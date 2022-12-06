b657d6e403c4163a80c250815fa5f829dd372608
Trevor Young <tbyoung99@tamu.edu>
2022-12-06 12:18:27 -0600

    modified:   AMARTensorFlowSetup.sh 	new file:   get_dependencies.sh

--- AMARTensorFlowSetup.sh
+++ AMARTensorFlowSetup.sh
@@ -1,8 +1,8 @@
-pip install tensorflow
-python3 -m pip install tflite-runtime
+sudo pip3 install tensorflow
+sudo pip3 install tflite-runtime
 sudo apt-get update
-sudo apt-get install python3-pycoral
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+sudo apt-get install python3-pycoral
 sudo apt-get update
 sudo apt-get install edgetpu-compiler
---
+++ get_dependencies.sh
@@ -0,0 +1,8 @@
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install python3-pip
+sudo apt-get remove numpy
+sudo pip3 install numpy
+sudo pip3 install git+https://github.com/pimoroni/VL53L0X-python
+sudo pip3 install smbus2
+sudo pip3 install pyusb
+git clone https://github.com/ansarid/pysicktim
